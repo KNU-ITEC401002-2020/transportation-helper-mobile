87c054d73fa61d78f84e2601a090b88b59c11c33
lilly35833 <62991885+lilly35833@users.noreply.github.com>
2020-12-13 21:01:40 +0900

    app

--- ShowActivity.java
+++ ShowActivity.java
@@ -4,22 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ShowActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        // 즐겨찾기 추가한 역
         ArrayList<String> list = new ArrayList<>();
             list.add("동대구 복합 환승센터") ;
             list.add("경북대학교 정문");
         }
 
-        // 리사이클러뷰에 LinearLayoutManager 객체 지정.
         RecyclerView recyclerView = findViewById(R.id.recycler1) ;
         recyclerView.setLayoutManager(new LinearLayoutManager(this)) ;
-
-        // 리사이클러뷰에 SimpleTextAdapter 객체 지정.
         adaptercd adapter = new adaptercd(list) ;
         recyclerView.setAdapter(adapter) ;
     }
